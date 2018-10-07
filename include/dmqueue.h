05b4af3dd6c3b8190108f74db1bd73a21a180150
brink.qiang <brink.qiang@gmail.com>
2018-10-07 22:34:31 +0800

    add

--- include/dmqueue.h
+++ include/dmqueue.h
@@ -1,5 +1,5 @@
 
-// Copyright (c) 2018 brinkqiang
+// Copyright (c) 2018 brinkqiang (brink.qiang@gmail.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -18,7 +18,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-
 
 #ifndef __DMQUEUE_H_INCLUDE__
 #define __DMQUEUE_H_INCLUDE__
