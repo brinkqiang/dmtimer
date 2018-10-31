58ab339960103ce4f53b281068d4a989ffdcaa43
brinkqiang <brink.qiang@gmail.com>
2018-10-31 18:19:24 +0800

    add

--- build.bat
+++ build.bat
@@ -1,6 +1,14 @@
+
+rem - clone code
+rem git clone https://github.com/brinkqiang/dmtimer.git
+rem pushd dmtimer
+rem git submodule update --init --recursive
+
 rmdir /S /Q build
 mkdir build
 pushd build
-cmake -DCMAKE_BUILD_TYPE=relwithdebinfo ..
-cmake --build .
+cmake -A x64 -DCMAKE_BUILD_TYPE=relwithdebinfo ..
+rem cmake --build .
 popd
+
+rem pause
