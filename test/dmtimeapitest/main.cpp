899e7d50a13832968ddc3105b2aa3727a6302706
brinkqiang <brink.qiang@gmail.com>
2019-09-26 17:38:24 +0800

    add

---
+++ test/dmtimeapitest/main.cpp
@@ -0,0 +1,97 @@
+
+#include "gtest.h"
+#include "dmutil.h"
+#include "dmtimermodule.h"
+#include <chrono>
+
+static inline uint32_t GetTickCount32_CLOCK_REALTIME_COARSE() {
+#ifdef _MSC_VER
+    return ::GetTickCount();
+#else
+    struct timespec ts = { 0 };
+    clock_gettime(CLOCK_REALTIME_COARSE, &ts);
+    return (ts.tv_sec * 1000 + ts.tv_nsec / 1000000);
+#endif
+}
+
+static inline uint32_t GetTickCount32_CLOCK_REALTIME() {
+#ifdef _MSC_VER
+    return ::GetTickCount();
+#else
+    struct timespec ts = { 0 };
+    clock_gettime(CLOCK_REALTIME, &ts);
+    return (ts.tv_sec * 1000 + ts.tv_nsec / 1000000);
+#endif
+}
+
+static inline uint32_t GetTickCount32_CLOCK_MONOTONIC() {
+#ifdef _MSC_VER
+    return ::GetTickCount();
+#else
+    struct timespec ts = { 0 };
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return (ts.tv_sec * 1000 + ts.tv_nsec / 1000000);
+#endif
+}
+
+static inline uint32_t GetTickCount32_chrono_system_clock() {
+    auto now = std::chrono::system_clock::now();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+}
+static inline uint32_t GetTickCount32_chrono_steady_clock() {
+    auto now = std::chrono::steady_clock::now();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+}
+
+static inline uint32_t GetTickCount32_high_resolution_clock() {
+    auto now = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count();
+}
+
+static inline uint32_t GetTickCount32_gettimeofday() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec * 1000 + tv.tv_usec / 1000;
+}
+
+#define TIME_TEST_COUNT (100000000)
+
+TEST(TIME_API, CLOCK_REALTIME_COARSE)
+{
+    uint64_t qwCount = 0;
+    for (int i=0; i < TIME_TEST_COUNT; ++i)
+    {
+        qwCount += GetTickCount32_CLOCK_REALTIME_COARSE();
+    }
+    std::cout << qwCount << std::endl;
+}
+
+TEST(TIME_API, CLOCK_REALTIME)
+{
+    uint64_t qwCount = 0;
+    for (int i = 0; i < TIME_TEST_COUNT; ++i)
+    {
+        qwCount += GetTickCount32_CLOCK_REALTIME();
+    }
+    std::cout << qwCount << std::endl;
+}
+
+TEST(TIME_API, CLOCK_MONOTONIC)
+{
+    uint64_t qwCount = 0;
+    for (int i = 0; i < TIME_TEST_COUNT; ++i)
+    {
+        qwCount += GetTickCount32_CLOCK_MONOTONIC();
+    }
+    std::cout << qwCount << std::endl;
+}
+
+TEST(TIME_API, gettimeofday)
+{
+    uint64_t qwCount = 0;
+    for (int i = 0; i < TIME_TEST_COUNT; ++i)
+    {
+        qwCount += GetTickCount32_gettimeofday();
+    }
+    std::cout << qwCount << std::endl;
+}
