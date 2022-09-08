22d82e972d96f4061e7e7a5e7c9ba30093efb371
brinkqiang <brink.qiang@gmail.com>
2022-09-08 20:45:34 +0800

    add

--- test/dmtimertest/main.cpp
+++ test/dmtimertest/main.cpp
@@ -25,7 +25,7 @@ class CMain : public IDMConsoleSink,
 #ifdef _DEBUG
         eMAX_PLAYER = 1 * 10000,
 #else
-        eMAX_PLAYER = 1 * 10000,
+        eMAX_PLAYER = 100 * 10000,
 #endif
 
         eMAX_PLAYER_EVENT = 10,
