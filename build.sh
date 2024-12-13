294ebd61893e0f4cf0ebf2764ccad0e5477dc408
brinkqiang <brrink.qiang@gmail.com>
2024-12-13 11:50:13 +0800

    --wip-- [skip ci]

--- build.sh
+++ build.sh
@@ -2,13 +2,15 @@
 
 rm -rf build
 mkdir -p build
+
+# pushd build
 cd build
 
 # -DCMAKE_EXPORT_COMPILE_COMMANDS=ON option is implemented only by Makefile Generators and Ninja Generators.
 cmake -DCMAKE_BUILD_TYPE=relwithdebinfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cmake --build . --config relwithdebinfo -- -j$(nproc)
-cd ..
 
 # popd
+cd ..
 
 # echo continue && read -n 1
