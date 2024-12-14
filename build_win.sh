a5d91a043e4c2fb983d541809fb84753312d1c31
brinkqiang <brink.qiang@gmail.com>
2024-12-14 18:17:52 +0800

    add

--- build_win.sh
+++ build_win.sh
@@ -8,7 +8,7 @@ cd build
 
 # -DCMAKE_EXPORT_COMPILE_COMMANDS=ON option is implemented only by Makefile Generators and Ninja Generators.
 cmake -DCMAKE_BUILD_TYPE=relwithdebinfo -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
-cmake --build . --config relwithdebinfo -- /m:%NUMBER_OF_PROCESSORS%
+cmake --build . --config relwithdebinfo -- /m:$(nproc)
 
 # popd
 cd ..
