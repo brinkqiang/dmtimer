51af3569fd346b66d8b1983db6888851f1318c42
brinkqiang <brink.qiang@gmail.com>
2025-01-02 14:15:57 +0800

    add

---
+++ build_android.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -rf build
+mkdir -p build
+
+# pushd build
+cd build
+
+# -DCMAKE_EXPORT_COMPILE_COMMANDS=ON option is implemented only by Makefile Generators and Ninja Generators.
+cmake -DCMAKE_BUILD_TYPE=relwithdebinfo -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake --build . --config relwithdebinfo -- -j$(nproc)
+
+# popd
+cd ..
+
+# echo continue && read -n 1
