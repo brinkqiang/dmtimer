09eeb3d17ed2c4c630e6f847ae5a9cb9dd6fdfb3
brinkqiang <brink.qiang@gmail.com>
2025-01-02 14:19:23 +0800

    add

--- build_android.sh
+++ build_android.sh
@@ -7,6 +7,11 @@ mkdir -p build
 cd build
 
 # -DCMAKE_EXPORT_COMPILE_COMMANDS=ON option is implemented only by Makefile Generators and Ninja Generators.
+echo download https://googledownloads.cn/android/repository/android-ndk-r27c-linux.zip
+echo x android-ndk-r27c-linux
+echo sudo mv android-ndk-r27c-linux /opt
+echo export ANDROID_NDK=/opt/android-ndk-r27c
+
 cmake -DCMAKE_BUILD_TYPE=relwithdebinfo -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cmake --build . --config relwithdebinfo -- -j$(nproc)
 
