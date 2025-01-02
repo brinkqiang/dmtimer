074797dc196f53f811c0d9c39dc2a3280e876112
brinkqiang <brink.qiang@gmail.com>
2025-01-02 14:22:18 +0800

    add

--- build_android.sh
+++ build_android.sh
@@ -12,7 +12,7 @@ echo x android-ndk-r27c-linux
 echo sudo mv android-ndk-r27c-linux /opt
 echo export ANDROID_NDK=/opt/android-ndk-r27c
 
-cmake -DCMAKE_BUILD_TYPE=relwithdebinfo -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -DCMAKE_BUILD_TYPE=relwithdebinfo -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK}/build/cmake/android.toolchain.cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cmake --build . --config relwithdebinfo -- -j$(nproc)
 
 # popd
